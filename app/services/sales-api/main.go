74e2f16f103c980d9503f6a1d4e82bf2792f1e58
Nathan Tsao <tw_lala@hotmail.com>
2023-03-14 21:46:47 +0800

    add configuration

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -1,23 +1,27 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+	"time"
 
+	"github.com/ardanlabs/conf/v2"
 	_ "go.uber.org/automaxprocs"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
+/*
+Need to figure out timeouts for http service.
+*/
+
 var build = "develop"
 
 func main() {
-	// Set the correct number of threads for the service
-	// based on what is available either by the machine or quotas.
-	// if _, err := maxprocs.Set(); err != nil {
-	// 	fmt.Printf("maxprocs: %v\n", err)
-	// 	os.Exit(1)
-	// }
 
 	// Construct the application logger.
 	log, err := initLogger("SALES-API")
@@ -35,6 +39,60 @@ func main() {
 }
 
 func run(log *zap.SugaredLogger) error {
+	// Set the correct number of threads for the service
+	// based on what is available either by the machine or quotas.
+	// if _, err := maxprocs.Set(); err != nil {
+	// 	return fmt.Errorf("maxprocs: %w", err)
+	// }
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	// =========================================================================
+	// Configuration
+	cfg := struct {
+		conf.Version
+		Web struct {
+			APIHost         string        `conf:"default:0.0.0.0:3000"`
+			DebugHost       string        `conf:"default:0.0.0.0:4000"`
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:10s"`
+			IdleTimeout     time.Duration `conf:"default:120s"`
+			ShutdownTimeout time.Duration `conf:"default:20s,mask"`
+		}
+	}{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "copyright information here",
+		},
+	}
+
+	const prefix = "SALES"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	// =========================================================================
+	// App Starting
+
+	log.Infow("starting service", "version", build)
+	defer log.Infow("shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	log.Infow("startup", "config", out)
+
+	// =========================================================================
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdown
+
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/wolftsao/service
 go 1.19
 
 require (
+	github.com/ardanlabs/conf/v2 v2.2.0
 	go.uber.org/automaxprocs v1.5.1
 	go.uber.org/zap v1.24.0
 )
